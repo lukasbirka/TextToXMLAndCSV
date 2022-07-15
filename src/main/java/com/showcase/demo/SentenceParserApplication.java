ed2374a92cadc546f511cd8cb39eff948678d0e2
lukasbirka <lukas.birka@gmail.com>
2022-07-15 16:14:51 +0200

    first commit

---
+++ src/main/java/com/showcase/demo/SentenceParserApplication.java
@@ -0,0 +1,13 @@
+package com.showcase.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentenceParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentenceParserApplication.class, args);
+	}
+
+}
